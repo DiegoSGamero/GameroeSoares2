ceaef45fa273f7a3c639712ca8ecdfcb985a0186
diego joao <diegosgjoao@gmail.com>
2023-11-10 10:12:01 -0300

    model admin and all controllers created, just missing the pages concerns

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'admins/index'
+  get 'admins/show'
+  get 'admins/new'
+  get 'admins/create'
+  get 'admins/edit'
+  get 'admins/update'
+  get 'admins/destroy'
+  get 'properties/index'
+  get 'properties/show'
+  get 'properties/new'
+  get 'properties/create'
+  get 'properties/edit'
+  get 'properties/update'
+  get 'properties/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_123254) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_131019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "full_name"
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
