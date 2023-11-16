b73b3ad92db83fd633da98d0bec0b77e2d623731
diego joao <diegosgjoao@gmail.com>
2023-11-16 10:26:31 -0300

    devise controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  devise_for :users, controllers: {
+   sessions: 'users/sessions'
+  }
   root to: "pages#home"
   get 'pages/about_us'
   get '/produtos', to: "pages#products"
