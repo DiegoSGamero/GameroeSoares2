bc6534eb878b449c275b9068de4d8abc2cd11a6e
diego joao <diegosgjoao@gmail.com>
2023-11-16 14:22:18 -0300

    still on devise configurations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.address = "#{params[:user][:street]}, #{params[:user][:number]}, #{params[:user][:neighborhood]}, #{params[:user][:city]}"
 
     if @user.save
       redirect_to new_session_path
-    else
-      render :new, status: :unprocessable_entity, alert: "Não foi possível processar seus dados"
     end
   end
 
@@ -31,7 +28,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:full_name, :email, :CPF, :RG, :street, :number, :neighborhood, :city)
+    params.require(:user).permit(:full_name, :email, :CPF, :address, :phone_number, :RG)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_and_belongs_to_many :properties
 
   validates :full_name, presence: true, format: { with: /\A\w+\s+\w+/ }
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :RG, presence: true, format: { with: /\A\d{1,2}\.\d{3}\.\d{3}-\d{1}\z/ }
-  validates :CPF, presence: true, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/ }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :RG, presence: true, format: { with: /\A\d{1,2}\.\d{3}\.\d{3}-\d{1}\z/ }, uniqueness: true
+  validates :CPF, presence: true, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/ }, uniqueness: true
   validates :phone_number, presence: true, format: { with: /\A\(\d{2}\) \d{5}-\d{4}\z/ }
   validates :address, presence: true
 
