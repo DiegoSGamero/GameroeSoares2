9cc14e97b53302f568b6e3e47261d7ac8717ab5c
diego joao <diegosgjoao@gmail.com>
2023-11-16 10:58:38 -0300

    keep working on registration feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :properties
+
+  validates :full_name, presence: true, format: { with: /\A\w+\s+\w+/ }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :RG, presence: true, format: { with: /\A\d{1,2}\.\d{3}\.\d{3}-\d{1}\z/ }
+  validates :CPF, presence: true, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/ }
+  validates :phone_number, presence: true, format: { with: /\A\(\d{2}\) \d{5}-\d{4}\z/ }
+  validates :address, presence: true
 end
