0023d79e45d9f5ee29af80e3a0d28808b4a43b97
diego joao <diegosgjoao@gmail.com>
2023-12-05 15:29:31 -0300

    validates for admin username

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,7 +6,9 @@ class Admin < ApplicationRecord
 
   validates :full_name, presence: true, format: { with: /\A[a-zA-Z]+\.?(\s[a-zA-Z]+\.?)+\z/, message: "O nome completo deve ter pelo menos 2 nomes" }
   validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true, format: { with: /\A(?=.*gamero)(?=.*\d)(?=.*[!@#$%^&*()-_=+{}\[\]|\\;:'",.<>/?`~]).*\z/ }
+
   validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}\z/, message: "Deve conter pelo menos uma letra e um número" }
-  
+
+  validates :username, presence: true, uniqueness: true, format: { with: /\A(?=.*gamero)(?=.*\d)(?=.*[!@#\$%^&*()-_=+{}\[\]|\\;:'",.<>\/\?`~]).*\z/ }
+
 end
