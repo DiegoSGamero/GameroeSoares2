9a09e03b04885165523be34a8942a97d8c7ad704
diego joao <diegosgjoao@gmail.com>
2023-12-08 10:14:42 -0300

    letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # Substitua com seu host local, se necessário
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
